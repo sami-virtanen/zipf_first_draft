a60545ef942023749a1d1bfe89f59bee9ff64227
Amira Khan <amira.zipfs@gmail.com>
2020-08-26 10:37:24 -0700

    Edit to plot frequency against rank on log-log axes

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -8,8 +8,8 @@ def main(args):
     df = pd.read_csv(args.infile, header=None, names=('word', 'word_frequency'))
     df['rank'] = df['word_frequency'].rank(ascending=False, method='max')
     df['inverse_rank'] = 1 / df['rank']
-    ax = df.plot.scatter(x='word_frequency', y='inverse_rank',
-                         figsize=[12, 6], grid=True)
+    ax = df.plot.scatter(x='word_frequency', y='rank', loglog=True,
+                         figsize=[12, 6], grid=True, xlim=args.xlim)
     ax.figure.savefig(args.outfile)
 
 
